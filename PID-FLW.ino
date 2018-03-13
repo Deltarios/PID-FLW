512621e17761bc3b6c4f81629d853e76c292a80e
Ariel Arturo Ríos Sierra <arturi.marking@gmail.com>
2018-03-12 23:08:43 -0600

    Se agrega el codigo principal
    Se agreca el codigo de logica del seguidor de linea

---
+++ PID-FLW.ino
@@ -0,0 +1,24 @@
+/*
+ * ***************CONTROL PID SEGUIDOR DE LINEA******************
+ * 
+ * AUTOR: ARIEL ARTURO RÌOS SIERRA
+ * 
+ * CORREO: arturi.marking@gmail.com
+ * 
+ * FECHA: 12 de marzo del 2018
+ * 
+ * VERSION: 1.0
+ */
+
+ #define 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
