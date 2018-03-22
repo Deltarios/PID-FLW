d8f74bd7bfaa4ed27962470b2b5a63753867b3ea
Ariel Arturo Ríos Sierra <arturi.marking@gmail.com>
2018-03-21 22:16:39 -0600

    Sistema nuevo de encendido
    Se cambio el sistema de encendido

--- PID-FLW.ino
+++ PID-FLW.ino
@@ -40,7 +40,7 @@ unsigned int valoresSensorIr[NUM_SENSOR_IR];
 bool estadoBoton = false;
 bool estadoRobot = false;
 
-const float velocidadBase = 150.0;
+const float velocidadBase = 100.0;
 
 const float kP = 0.0;
 const float kI = 0.0;
@@ -49,6 +49,18 @@ const float kD = 0.0;
 volatile unsigned long leftCount = 0;
 volatile unsigned long rightCount = 0;
 
+float rpmIzq = 0;
+float rpmDer = 0;
+float velocidadIzq = 0;
+float velocidadDer = 0;
+
+unsigned long tiempoAnterior = 0;
+unsigned int pulsosPorRevolucion = 8;
+
+unsigned int diametroRueda = 20;
+int relacionRuedas = 10;
+
+unsigned int posicion_actual = 0;
 unsigned int posicion = 0;
 unsigned int posicionAnterior = 0;
 unsigned int derivativo = 0;
@@ -87,50 +99,6 @@ void setup() {
   digitalWrite(STBY, LOW);
 }
 
-void loop() {
-
-  estadoBoton = digitalRead(BUTTON_STATUS);
-
-  if (estadoBoton) {
-    estadoRobot = !estadoRobot;
-    delay(40);
-  }
-
-  if (!estadoRobot) {
-    digitalWrite(STBY, LOW);
-    loop();
-  }
-
-  inicioRobot();
-}
-
-void inicioRobot() {
-  digitalWrite(STBY, HIGH);
-
-  int errorPID = calculoPID;
-
-  posicionAnterior = posicion;
-}
-
-unsigned int calculoPID() {
-  posicion = leerPosicionError();
-
-  derivativo = posicion - posicionAnterior;
-
-  integral = integral + posicion;
-
-  return (kP * posicion + kI * integral + kD * derivativo);
-
-}
-
-unsigned int leerPosicionError() {
-  qtra.read(valoresSensorIr);
-
-  unsigned int posicion_actual = qtra.readLine(valoresSensorIr, QTR_EMITTERS_ON, 0);
-
-  return (POS_OBJECTIVO - posicion_actual);
-}
-
 void leftEncoderEvento() {
   if (digitalRead(ENCO_A_IZQ) == HIGH) {
     if (digitalRead(ENCO_B_IZQ) == LOW) {
@@ -161,6 +129,94 @@ void rightEncoderEvento() {
       rightCount++;
     }
   }
+}
+
+void loop() {
+
+  estadoBoton = digitalRead(BUTTON_STATUS);
+
+  if (estadoBoton) {
+    estadoRobot = !estadoRobot;
+    delay(40);
+  }
+
+  if (!estadoRobot) {
+    if (STBY == HIGH) {
+      digitalWrite(STBY, LOW);
+    }
+    loop();
+  }
+
+  inicioRobot();
+}
+
+void inicioRobot() {
+  digitalWrite(STBY, HIGH);
+
+  leerVelocidades();
+
+  int errorPID = calculoPID;
+
+  float velocidadActualIzq = velocidadIzq - errorPID;
+  float velocidadActualDer = velocidadDer + errorPID;
+
+  float velocidadRPMIzq = rpmIzq - errorPID;
+  float velocidadRPMDer = rpmDer + errorPID;
+
+  float potenciaPWMIzq = map(velocidadRPMIzq, 0, 3000, 0, 255);
+  float potenciaPWMDer = map(velocidadRPMDer, 0, 3000, 0, 255);
+
+  if (potenciaPWMIzq >= velocidadBase) {
+    potenciaPWMIzq = velocidadBase;
+  }
+
+  if (potenciaPWMDer >= velocidadBase) {
+    potenciaPWMDer = velocidadBase;
+  }
+
+  analogWrite(PWMA, potenciaPWMDer);
+  adelante();
+
+  posicionAnterior = posicion;
+}
+
+void leerVelocidades() {
+  long tiempoActual = millis();
+  if (tiempoActual - tiempoAnterior >= 1000) {
+    noInterrupts();
+    rpmIzq = 60 * leftCount / pulsosPorRevolucion * 1000 / (millis() - tiempoAnterior);
+
+    rpmDer = 60 * rightCount / pulsosPorRevolucion * 1000 / (millis() - tiempoAnterior);
+
+    velocidadIzq = rpmIzq / relacionRuedas * 3.1416 * diametroRueda * 60 / 1000000;
+
+    velocidadIzq = rpmDer / relacionRuedas * 3.1416 * diametroRueda * 60 / 1000000;
+
+    leftCount = 0;
+    rightCount = 0;
+
+    tiempoAnterior = millis();
+    interrupts();
+  }
+}
+
+unsigned int calculoPID() {
+  posicion = leerPosicionError();
+
+  derivativo = posicion - posicionAnterior;
+
+  integral = integral + posicion;
+
+  return (kP * posicion + kI * integral + kD * derivativo);
+
+}
+
+unsigned int leerPosicionError() {
+  qtra.read(valoresSensorIr);
+
+  posicion_actual = qtra.readLine(valoresSensorIr, QTR_EMITTERS_ON, 0);
+
+  return (POS_OBJECTIVO - posicion_actual);
 }
 
 void adelante() {
